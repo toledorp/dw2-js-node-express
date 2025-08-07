96e2e9ea2f254074fc9460af03d9cf78a687ca6f
toledorp <roge.pupo.toledo@gmail.com>
2025-08-07 18:59:25 -0300

    VAR/LET/CONST

--- JavaScript/Aula-01.1/js-01.1.js
+++ JavaScript/Aula-01.1/js-01.1.js
@@ -1,3 +1,16 @@
+/* Variáveis podem ser declaradas de 03 formas:
+* Var / Let e Const
+Var : no geral evite o seu uso, pode nao ser muito seguro. Pode gerar duplicidade e com isso perde uma das variaveis
+Let : Utilize quando for necessário reatribuir o valor da variável (declaração x atribuição)
+      Declaração você cria uma variavel e deixa ela ja reservada. Atribuir é quando você atribui um valor para ela. 
+      Quando o valor precisa ser reatribuido
+Const : Não consegue reatribuir o valor. O valor fica fixo. Utilize quando nãop precisdar reatribuir o valor da variavel
+        Ex. const cidade = "registro" - OK 
+            const cidade = "pariquera" - não pode, o vlaor não pode ser reatribuido
+
+*/
+
+
 //Tipos de funções
 
 // Função Simples
